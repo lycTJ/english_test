985aa72bd9865fa42bbbb4cb30e9dd321d9527a0
lycTJ <f29005700@gmail.com>
2022-03-08 14:02:06 +0800

    Add gitignore

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const expressLayouts = require('express-ejs-layouts');
+// const fileUpload = require('express-fileupload');
+// const session = require('express-session');
+// const cookieParser = require('cookie-parser');
+// const flash = require('connect-flash');
+const fs = require("fs")
+
+const app = express()
+
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname+'public/css'))
+
+
+app.use(expressLayouts)
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');  
+
+
+
+
+
+
+var file = "data.json"
+
+var result = JSON.parse(fs.readFileSync(file))
+var fir = result
+var strResult = JSON.stringify(fir)
+
+
+
+app.get("/", (req,res)=>{
+    console.log("->",req.url)
+    
+    
+    // res.send(strResult)
+    
+    res.render("index",{title: "English-testing",parm:result})
+})
+
+
+
+
+
+
+
+
+/**Server連線 
+ * 
+*/
+const PORT = process.env.port || 3000
+const IP = "127.0.0.1"
+app.listen(PORT,(err)=>{
+    if(err){
+        console.log("wrong")
+    }
+    console.log(`Server is running at http://${IP}:${PORT}`)
+})
