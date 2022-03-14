d01ea9bec289f9d64ed159cbc8dbfdf7eff1e193
lycTJ <f29005700@gmail.com>
2022-03-14 14:46:09 +0800

    Add judge Answer function

--- public/js/script.js
+++ public/js/script.js
@@ -5,18 +5,45 @@ let dataLen = document.getElementsByClassName("dataLen")
 let btn = document.getElementById("btnAns");
 btn.addEventListener("click", () => {
     showDiv();
+    submit()
 });
 
-
+function submit() {
+    for (var i = 0; i < dataLen.length; i++) {
+        let ans = document.getElementById(`ans${i}`)
+        var obj=document.getElementsByName(`radio${i}`);
+        
+        
+        var selected=[];
+        for (var j=0; j<obj.length; j++) {
+            if (obj[j].checked) {
+            selected.push(obj[j].value);
+            }
+            
+        }
+        
+        //   alert("您勾選的項目 : " + selected.join());
+        if (selected.join() == ans.textContent){
+            ans.style.color= "green"
+        }
+        else{
+            ans.style.color= "red"
+        }
+    }
+};
 
 function showDiv() {
 for (var i = 0; i < dataLen.length; i++) {
-    let ans = document.getElementById("ans")
-    let radio = document.getElementsByName(`input[name="radio${i}"`);
-    let radioVal = document.getElementsByName(`input[name="radio${i}":checked`).value;
+    
+    let r = document.querySelector(`[name=radio${i}]:checked`)
+    
+
+    let radioVal = document.getElementsByName(`radio0`).value;
     var showDiv = document.getElementById("showDiv" + i);
 
     if (showDiv.style.display === "none") {
+        
+        
         // 處理介面
         // if(radioVal == ans){
         //     radio.style.backgroundColor = "red"
